d63ff751a30ed8d80e9271acd32468682c58a853
Дмитрий Пантелеев <d.panteleev@profsoft.pro>
2024-04-21 20:36:28 +0400

    UPD: NetworkEvent is Equatable

--- Sources/AsyncNetwork/Plugins/Events/NetworkEvent.swift
+++ Sources/AsyncNetwork/Plugins/Events/NetworkEvent.swift
@@ -22,3 +22,29 @@ public enum NetworkEvent {
     /// Служит для оповещения пользователя о вторичной информации
     case custom(_: String)
 }
+
+extension NetworkEvent {
+    public var description: String {
+        switch self {
+        case .initial(let id):
+            "\(id) inited"
+        case .request(let r):
+            "Request:\n\(r.cURL)"
+        case .response(let r):
+            "Response:\n\(r.response)"
+        case .error(let e):
+            "Error:\n\(e.description)"
+        case .custom(let s):
+            s
+        }
+    }
+}
+
+extension NetworkEvent: Equatable {
+    public static func == (lhs: NetworkEvent, rhs: NetworkEvent) -> Bool {
+        switch (lhs, rhs) {
+        case (let l, let r):
+            l.description == r.description
+        }
+    }
+}
